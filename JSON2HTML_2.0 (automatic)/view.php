3e9f2a1472754046815acf1ef4c6eee0bcc0a52d
ninock2 <cehteljkristl.nino@gmail.com>
2021-07-25 18:07:32 +0200

    Automatic converter from JSON2HTML added

---
+++ JSON2HTML_2.0 (automatic)/view.php
@@ -0,0 +1,80 @@
+<html>
+    <head>
+        <title>JSON to HTML converter</title>
+        <style>
+            html {
+                height: 100%;
+                background-color: #f7f2e7;
+            }
+            body {
+                background-color: #f7f2e7;
+                font-family: "Trebuchet MS", Helvetica, sans-serif;
+                text-align: left;
+            }
+            table {
+                border: 1px solid #aaa;
+                margin: 20px;
+            }
+            tr:nth-child(even) {
+                background: #ccc;
+            }
+            tr:nth-child(odd) {
+                background: #eee;
+            }
+            td {
+                padding: 3px 15px;
+                vertical-align: top;
+            }
+            .btnsubmit {
+                width: 200px;
+                height: 30px;
+                background-color: #ccc;
+                border: 1px solid #aaa;
+                font-size: 17px;
+                color: #333;
+            }
+            .btnsubmit:hover {
+                cursor: pointer;
+                background-color: #aaa;
+                border: 1px solid #888;
+                color: #eee;
+                text-shadow: 0 0 15px black;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>JSON to HTML converter</h1>
+        <div class="col-sm-6">
+        <form action="#" method="post">
+            <input type="hidden" name="action" value="importcsv">
+            <table>
+                <tr>
+                    <td colspan="2" align=center><h3>JSON datoteka</h3> Ime datoteke: <?=(isset($_POST['filepath'])) ? $_POST['filepath'] : ""?></td>
+                </tr>
+                <tr>
+                    <td>Pot datoteke</td>
+                    <td><input type="file" name="filepath" id="filepath" value="<?= (isset($_POST['filepath'])) ? $_POST['filepath'] : "" ?>" required></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align=center>
+                        <br>
+                        <input type="submit" value="Uvozi" class="btnsubmit">
+                        <input type="button" value="Novo" class="btnsubmit" onclick="window.location=''">
+                        <br><br>
+                    </td>
+                </tr>
+            </table>
+        </form>
+        </div>
+        <div class="col-sm-6">
+            <hr>
+            <h4>Predogled vsebine generirane HTML datoteke: </h4>
+        <?php
+            //Ustvarjen nov objekt in klicanje celotne funkcije
+            $newObj = new json2html;
+            $newObj->convert(); 
+        ?>
+        </div>
+    </body>
+</html>    
+   
